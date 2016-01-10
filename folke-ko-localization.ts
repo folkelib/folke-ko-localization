6ba5df57f9d1b9b41e6a93a88a30fb92c5f44b84
Sidoine De Wispelaere <sidoine@sidoine.net>
2016-01-10 19:07:20 +0100

    Add a register method

--- folke-ko-localization.ts
+++ folke-ko-localization.ts
@@ -12,70 +12,73 @@ for (var key in text) {
     }
 }
 
-// A binding handler that set the text content with a format ({n} is replaced by the nth parameter)
-ko.bindingHandlers['locf'] = {
-    update: (element: HTMLElement, valueAccessor: () => any[]) => {
-        var parameters = valueAccessor();
-        var text = keys[parameters[0]];
-        for (var i = 1; i < parameters.length; i++) {
-            text = text.replace('{' + (i - 1) + '}', ko.unwrap(parameters[i]));
-        }
-        element.innerHTML = text;
-    }
-}
-
-// A binding handler to set attribute values
-ko.bindingHandlers['loca'] = {
-    update: (element: HTMLElement, valueAccessor: () => any) => {
-        var parameters = valueAccessor();
-        for (var key in parameters) {
-            var value = parameters[key];
-            element.setAttribute(key, keys[value]);
+export function register() {
+    // A binding handler that set the text content with a format ({n} is replaced by the nth parameter)
+    ko.bindingHandlers['locf'] = {
+        update: (element: HTMLElement, valueAccessor: () => any[]) => {
+            var parameters = valueAccessor();
+            var text = keys[parameters[0]];
+            for (var i = 1; i < parameters.length; i++) {
+                text = text.replace('{' + (i - 1) + '}', ko.unwrap(parameters[i]));
+            }
+            element.innerHTML = text;
         }
     }
-};
 
-ko.bindingProvider.instance['preprocessNode'] = function (node: Node) {
-    if (node.nodeType === node.ELEMENT_NODE) {
-        var element = <HTMLElement>node;
-        for (var i = 0; i < element.attributes.length; i++) {
-            var attribute = element.attributes[i];
-            var value = attribute.value;
-            if (value.indexOf("{{") == 0) {
-                value = value.substr(2, value.length - 4);
-                element.removeAttribute(attribute.name);
-                if (element.hasAttribute('data-bind')) {
-                    element.attributes.getNamedItem('data-bind').value += ',loca: {' + attribute.name + ':"' + value + '"}';
+    // A binding handler to set attribute values
+    ko.bindingHandlers['loca'] = {
+        update: (element: HTMLElement, valueAccessor: () => any) => {
+            var parameters = valueAccessor();
+            for (var key in parameters) {
+                var value = parameters[key];
+                element.setAttribute(key, keys[value]);
+            }
+        }
+    };
+
+    ko.bindingProvider.instance['preprocessNode'] = function (node: Node) {
+        if (node.nodeType === node.ELEMENT_NODE) {
+            var element = <HTMLElement>node;
+            for (var i = 0; i < element.attributes.length; i++) {
+                var attribute = element.attributes[i];
+                var value = attribute.value;
+                if (value.indexOf("{{") == 0) {
+                    value = value.substr(2, value.length - 4);
+                    element.removeAttribute(attribute.name);
+                    if (element.hasAttribute('data-bind')) {
+                        element.attributes.getNamedItem('data-bind').value += ',loca: {' + attribute.name + ':"' + value + '"}';
+                    }
+                    else {
+                        element.setAttribute('data-bind', 'loca: {' + attribute.name + ':"' + value + '"}');
+                    }
+                }
+            }
+        }
+
+        if (node.nodeType === 3) {
+
+            if (node.nodeValue && node.nodeValue.indexOf('{{nohtml|') !== -1) {
+                node.nodeValue = node.nodeValue.replace(/{{nohtml\|(.*?)}}/, (match, id) => keys[id] || id);
+                return [node];
+            }
+            else if (node.nodeValue && node.nodeValue.indexOf('{{') !== -1) {
+                if (node.nodeValue.indexOf(',') !== -1) {
+                    var newElement = document.createElement('span');
+                    var value = node.nodeValue.match(/{{(.*?)}}/)[1];
+                    var parameters = value.split(',');
+                    parameters[0] = "'" + parameters[0] + "'";
+                    newElement.setAttribute('data-bind', 'locf: [' + parameters.join(',') + ']');
+                    node.parentNode.replaceChild(newElement, node);
+                    return [newElement];
                 }
                 else {
-                    element.setAttribute('data-bind', 'loca: {' + attribute.name + ':"' + value + '"}');
+                    var newElement = document.createElement('span');
+                    newElement.innerHTML = node.nodeValue.replace(/{{(.*?)}}/, (match, id) => keys[id] || id);
+                    node.parentNode.replaceChild(newElement, node);
+                    return [newElement];
                 }
             }
         }
     }
 
-    if (node.nodeType === 3) {
-
-        if (node.nodeValue && node.nodeValue.indexOf('{{nohtml|') !== -1) {
-            node.nodeValue = node.nodeValue.replace(/{{nohtml\|(.*?)}}/, (match, id) => keys[id] || id);
-            return [node];
-        }
-        else if (node.nodeValue && node.nodeValue.indexOf('{{') !== -1) {
-            if (node.nodeValue.indexOf(',') !== -1) {
-                var newElement = document.createElement('span');
-                var value = node.nodeValue.match(/{{(.*?)}}/)[1];
-                var parameters = value.split(',');
-                parameters[0] = "'" + parameters[0] + "'";
-                newElement.setAttribute('data-bind', 'locf: [' + parameters.join(',') + ']');
-                node.parentNode.replaceChild(newElement, node);
-                return [newElement];
-            }
-            else {
-                var newElement = document.createElement('span');
-                newElement.innerHTML = node.nodeValue.replace(/{{(.*?)}}/, (match, id) => keys[id] || id);
-                node.parentNode.replaceChild(newElement, node);
-                return [newElement];
-            }
-        }
-    }
 }
